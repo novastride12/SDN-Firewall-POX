b885003ad016598a8e6a75e75f8a4d85736940a9
novastride12 <aryavd12@gmail.com>
2026-04-04 00:08:44 +0530

    added firewall implementation

---
+++ firewall.py
@@ -0,0 +1,20 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+
+log = core.getLogger()
+
+def _handle_PacketIn(event):
+    packet = event.parsed
+    in_port = event.port
+
+    if in_port == 1:
+        print("Blocked packet from h1")
+        return
+
+    msg = of.ofp_packet_out()
+    msg.data = event.ofp
+    msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+    event.connection.send(msg)
+
+def launch():
+    core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
