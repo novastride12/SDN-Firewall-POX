26be362ee8fc1efc020e5304b41f15dbc6a32073
NOVA <aryavd12@gmail.com>
2026-04-09 23:51:02 +0530

    updated the firewall.py to filter the traffic based on ip address instead of port number

--- firewall.py
+++ firewall.py
@@ -5,12 +5,27 @@ log = core.getLogger()
 
 def _handle_PacketIn(event):
     packet = event.parsed
-    in_port = event.port
 
-    if in_port == 1:
-        print("Blocked packet from h1")
-        return
+    
+    ip_packet = packet.find('ipv4')
 
+    if ip_packet:
+        src_ip = ip_packet.srcip
+
+        
+        if str(src_ip) == "10.0.0.1":
+            log.info("Blocked packet from IP %s", src_ip)
+
+            # Install drop rule in switch
+            msg = of.ofp_flow_mod()
+            msg.match.dl_type = 0x0800   # IPv4
+            msg.match.nw_src = ip_packet.srcip
+            msg.actions = []  # no action = drop
+
+            event.connection.send(msg)
+            return
+
+    # Allow other traffic
     msg = of.ofp_packet_out()
     msg.data = event.ofp
     msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
